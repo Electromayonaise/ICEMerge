d84eb8b360f64ac838e3edba9c338d62752c20ad
Martín Gómez <123976183+Electromayonaise@users.noreply.github.com>
2024-04-23 12:44:41 -0500

    Upload gradle init and project blueprint

---
+++ server/src/main/java/Server.java
@@ -0,0 +1,15 @@
+import com.zeroc.Ice.*;
+import com.zeroc.Ice.Object;
+
+public class Server {
+    
+    public static void main(String[] args) {
+        try(com.zeroc.Ice.Communicator communicator = com.zeroc.Ice.Util.initialize(args, "server.config")) {
+            ObjectAdapter adapter = communicator.createObjectAdapter("services");
+            Object object = new BucketSort();
+            adapter.add(object, Util.stringToIdentity("ICEBucket"));
+            adapter.activate();
+            communicator.waitForShutdown();
+        }
+    }
+}
