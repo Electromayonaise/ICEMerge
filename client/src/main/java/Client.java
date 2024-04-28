2507cc0689c0819db38d2f8fa2e71431dde9d611
Electromayonaise <agazazaca@gmail.com>
2024-04-28 00:03:11 -0500

    Client now may enter a filepath to sort a desired file, added sample files for quick testing

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -1,59 +1,104 @@
 import BucketICE.*;
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
 import com.zeroc.Ice.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
 import java.util.concurrent.CompletableFuture;
-import java.util.random.*;
-
+import java.io.IOException;
+import java.util.Arrays;
+import java.io.IOException;
 
 public class Client {
 
-    Scanner scanner;
-
-    public static void main(String[] args) throws java.lang.Exception {
-        try(Communicator communicator = Util.initialize(args, "client.config")) {
+    public static void main(String[] args) {
+        try (Communicator communicator = Util.initialize(args, "client.config")) {
             ObjectPrx base = communicator.propertyToProxy("primaryServer.proxy");
             DistributedSortingPrx sorter = DistributedSortingPrx.checkedCast(base);
-            if(sorter == null) {
+            if (sorter == null) {
                 throw new Error("Invalid proxy");
             }
             System.out.println("Connected to the server");
             Scanner scanner = new Scanner(System.in);
             boolean exit = false;
-            while(!exit){
-                System.out.println("1. Sort a ran array of a given size");
-                System.out.println("2. Exit");
-                int choice = scanner.nextInt();
-                double a, b;
-                switch(choice) {
-                    case 1:
-                        System.out.println("Enter the size");
-                        int size=scanner.nextInt();
-                       //  System.out.println(Arrays.toString( (int[])sorter.getArr() ));
-                       int[] arr = new Random().ints(size, 1, 100).toArray();
-                       long startTime = System.currentTimeMillis();
-                        CompletableFuture<int[]> answer2= sorter.sortAsync(arr);
-                        int[] result=(int[])answer2.get();
-                        long endTime = System.currentTimeMillis();
-                        long duration = endTime - startTime;
-                        System.out.println("BEFORE");
-                        System.out.println(Arrays.toString(arr));
-                        System.out.println("AFTER");
-                        System.out.println(Arrays.toString(result) );
-                        System.out.println("DURATION IN ML"+ duration);
-                        
-                        break;
-                    case 2:
+            while (!exit) {
+                try {
+                    System.out.println("Select an option:");
+                    System.out.println("1. Enter the path of the file to be sorted");
+                    System.out.println("2. Sort sample files");
+                    int option = Integer.parseInt(scanner.nextLine());
+                    int[] numbers = null;
+                    switch (option) {
+                        case 1:
+                            System.out.println("Enter the path of the file to be sorted:");
+                            String path = scanner.nextLine();
+                            byte[] content = Files.readAllBytes(Paths.get(path));
+                            numbers = convertBytesToIntegers(content);
+                            break;
+                        case 2:
+                            System.out.println("Select a sample file:");
+                            System.out.println("1. numbers1.txt");
+                            System.out.println("2. numbers2.txt");
+                            System.out.println("3. numbers3.txt");
+                            System.out.println("4. numbers4.txt");
+                            System.out.println("5. numbers5.txt");
+                            int sampleFile = Integer.parseInt(scanner.nextLine());
+                            switch (sampleFile) {
+                                case 1:
+                                    numbers = convertBytesToIntegers(Files.readAllBytes(Paths.get("SampleFiles/numbers1.txt")));
+                                    break;
+                                case 2:
+                                    numbers = convertBytesToIntegers(Files.readAllBytes(Paths.get("SampleFiles/numbers2.txt")));
+                                    break;
+                                case 3:
+                                    numbers = convertBytesToIntegers(Files.readAllBytes(Paths.get("SampleFiles/numbers3.txt")));
+                                    break;
+                                case 4:
+                                    numbers = convertBytesToIntegers(Files.readAllBytes(Paths.get("SampleFiles/numbers4.txt")));
+                                    break;
+                                case 5:
+                                    numbers = convertBytesToIntegers(Files.readAllBytes(Paths.get("SampleFiles/numbers5.txt")));
+                                    break;
+                                default:
+                                    System.out.println("Invalid option");
+                                    break;
+                            }
+                    
+                            break;
+                        default:
+                            System.out.println("Invalid option");
+                            break;
+                    }
+                    long startTime = System.currentTimeMillis();
+                    CompletableFuture<int[]> sortedContentFuture = sorter.sortAsync(numbers);
+                    int[] sortedContent = sortedContentFuture.get();
+                    long endTime = System.currentTimeMillis();
+                    long duration = endTime - startTime;
+                    System.out.println("Sorted content:");
+                    System.out.println(Arrays.toString(sortedContent));
+                    System.out.println("Duration in milliseconds: " + duration);
+
+                    System.out.println("Do you want to enter another file? (Y/N)");
+                    String choice = scanner.nextLine();
+                    if (!choice.equalsIgnoreCase("Y")) {
                         exit = true;
-                        break;
-
-                    default:
-                        System.out.println("Invalid choice");
-
+                    }
+                } catch (IOException e) {
+                    System.err.println("Error reading file: " + e.getMessage());
+                } catch (java.lang.Exception e) {
+                    e.printStackTrace();
                 }
             }
-            scanner.close();
+        } catch (java.lang.Exception e) {
+            e.printStackTrace();
         }
     }
-} 
+
+    private static int[] convertBytesToIntegers(byte[] bytes) {
+        String[] numbersAsString = new String(bytes).split("\\s+");
+        int[] numbers = new int[numbersAsString.length];
+        for (int i = 0; i < numbersAsString.length; i++) {
+            numbers[i] = Integer.parseInt(numbersAsString[i]);
+        }
+        return numbers;
+    }
+}
